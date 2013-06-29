394bc3b0c07f0b00dfdb51097bdfa1641d3eae88
Noah Watkins <noahwatkins@gmail.com>
2013-06-29 08:49:43 -0700

    www: setup Django development env settings
    The development environment uses sqlite3, and once we create a
    production environment, we'll split up the settings files to easily
    switch between dev / prod modes.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- webapp/calamari/calamari/settings.py
+++ webapp/calamari/calamari/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'ceph',
 )
 
 # A sample logging configuration. The only tangible logging
