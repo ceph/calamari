243029f398eaa502c7031645f23ace822fc8ae69
Yan-Fa Li <yanfali@gmail.com>
2013-07-27 12:24:18 -0700

    Add a loading spinner to all the settings page
    Some of the ajax requests may take some time to complete.
    Provide an animation to keep the user aware something is happening.
    
    The font-heights of font-awesome are at odds with the general fonts, so
    use an invisible icon to keep the layout from jumping around
    when adding and removing the spinner animation via ng-show/ng-hide.

--- ui/admin/app/scripts/controllers/cluster.js
+++ ui/admin/app/scripts/controllers/cluster.js
@@ -3,8 +3,10 @@
 var clusterController = function($rootScope, $scope, $http, menus) {
         $scope.title = $rootScope.pageTitle;
         $scope.menus = menus.menu('cluster');
+        $scope.loading = true;
         $http.get('api/v1/cluster').success(function(data) {
             $scope.clusters = data;
+            $scope.loading = false;
         });
     };
 angular.module('adminApp').controller('ClusterCtrl', ['$rootScope', '$scope', '$http', 'menus', clusterController]);
--- ui/admin/app/scripts/controllers/general.js
+++ ui/admin/app/scripts/controllers/general.js
@@ -3,8 +3,10 @@
 var generalController = function($rootScope, $scope, $http, menus) {
         $scope.title = $rootScope.pageTitle;
         $scope.menus = menus.menu('general');
+        $scope.loading = true;
         $http.get('api/v1/general').success(function(data) {
             $scope.general = data;
+            $scope.loading = false;
         });
     };
 angular.module('adminApp').controller('GeneralCtrl', ['$rootScope', '$scope', '$http', 'menus', generalController]);
