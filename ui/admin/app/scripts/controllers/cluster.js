ffd1f63e688180a81e1766d2adcec50a2fc45b5a
Yan-Fa Li <yanfali@gmail.com>
2013-07-30 13:28:56 -0700

    AngularJS Form validation on Add
    - set type to URL
    - require both fields be set
    - skeleton function to send to server
    
    Add skeleton remove dialog
    
    - new template
    - new controller
    - remove button now active
    - icon changed to trash can

--- ui/admin/app/scripts/controllers/cluster.js
+++ ui/admin/app/scripts/controllers/cluster.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var clusterController = function($rootScope, $scope, $http, $timeout) {
+var clusterController = function($rootScope, $scope, $http, $timeout, $dialog) {
         $scope.title = $rootScope.pageTitle;
         $rootScope.activeTab = 'cluster';
         $scope.loaded = false;
@@ -9,19 +9,43 @@ var clusterController = function($rootScope, $scope, $http, $timeout) {
                 $scope.loading = true;
             }
         }, 125);
-        $http.get('/api/v1/cluster').success(function(data) {
-            if (data.length === 0) {
-                $scope.addClusterOpen = true;
-            }
-            $scope.clusters = data;
-            $scope.loaded = true;
-            $scope.loading = false;
-        });
+        var refresh = function() {
+                $http.get('/api/v1/cluster').success(function(data) {
+                    if (data.length === 0) {
+                        $scope.addClusterOpen = true;
+                    }
+                    $scope.clusters = data;
+                    $scope.loaded = true;
+                    $scope.loading = false;
+                });
+            };
+        refresh();
         $scope.openAdd = function() {
             $scope.addClusterOpen = true;
         };
         $scope.closeAdd = function() {
             $scope.addClusterOpen = false;
+        };
+        $scope.clusterAdd = function(cluster) {
+            console.log('Adding ' + cluster);
+            $scope.addClusterOpen = false;
+            $scope.loading = true;
+            $scope.allDisabled = true;
+            $http({
+                method: 'POST',
+                url: '/api/v1/cluster',
+                data: JSON.stringify({
+                    'name': cluster.name,
+                    'api_base_url': cluster.uri
+                })
+            }).success(function() {
+                refresh();
+                $scope.allDisabled = false;
+                $scope.loading = false;
+            }).error(function() {
+                $scope.allDisabled = false;
+                $scope.loading = false;
+            });
         };
         $scope.openEdit = function() {
             $scope.cluster = {
@@ -37,5 +61,39 @@ var clusterController = function($rootScope, $scope, $http, $timeout) {
             backdropFade: true,
             dialogFade: true
         };
+        $scope.dialogOpts = {
+            backdrop: true,
+            keyboard: true,
+            backdropClick: true,
+            templateUrl: 'views/removeCluster.html',
+            controller: 'RemoveDialogController',
+            resolve: {
+                cluster: function() {
+                    return {
+                        name: 'my cluster',
+                        id: 10
+                    };
+                }
+            }
+        };
+        $scope.openRemove = function() {
+            var d = $dialog.dialog($scope.dialogOpts);
+            d.open().then(function(result) {
+                //$scope.loading = true;
+                console.log(result);
+
+            });
+        };
     };
-angular.module('adminApp').controller('ClusterCtrl', ['$rootScope', '$scope', '$http', '$timeout', clusterController]);
+angular.module('adminApp').controller('ClusterCtrl', ['$rootScope', '$scope', '$http', '$timeout', '$dialog', clusterController]);
+
+var removeDialogController = function($scope, dialog, cluster) {
+        $scope.cluster = cluster;
+        $scope.closeRemove = function() {
+            dialog.close({});
+        };
+        $scope.clusterRemove = function(cluster) {
+            dialog.close(cluster);
+        };
+    };
+angular.module('adminApp').controller('RemoveDialogController', ['$scope', 'dialog', 'cluster', removeDialogController]);
