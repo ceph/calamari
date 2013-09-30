3dc2c28ba3f86c0fad85b061c901cf4043f7a775
Noah Watkins <noahwatkins@gmail.com>
2013-09-30 14:34:14 -0700

    settings.py: log errors to django log file

--- webapp/calamari/calamari/settings.py
+++ webapp/calamari/calamari/settings.py
@@ -170,21 +170,19 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'log_file': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/opt/calamari/log/django.log'
+        },
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['log_file', 'mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
