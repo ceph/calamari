28bb94ed335ea50962df3519e9e8a8a8ff8cbfbf
Yan-Fa Li <yanfali@gmail.com>
2013-10-03 15:49:02 -0700

    Implement User settings page network code
    Stubbed implementation. Add a working xhr call to update the user
    settings page. Backend may not yet be working.

--- ui/admin/app/scripts/controllers/user.js
+++ ui/admin/app/scripts/controllers/user.js
@@ -1,22 +1,64 @@
+/* global _ */
 'use strict';
 
-var userController = function($rootScope, $scope) {
+var userController = function($rootScope, $scope, userSrv) {
         $rootScope.activeTab = 'user';
         $scope.title = $rootScope.pageTitle;
         $scope.dashboard = $rootScope.dashboard;
-        $scope.clear = function() {
-            $scope.user.email = 'calamari@inktank.com';
-            $scope.user.password = '';
-            $scope.user.displayName = 'Calamari Admin';
-            $scope.user.confirm = '';
-        };
-        $scope.user = {
-            name: 'admin',
+        var userDefaults = {
+            username: 'admin',
             email: 'calamari@inktank.com',
             displayName: 'Calamari Admin',
             password: '',
             confirm: ''
         };
+        $scope.user = {};
+        angular.extend($scope.user, userDefaults);
+
+        function clear() {
+            angular.extend($scope.user, userDefaults);
+        }
+
+        function update(user) {
+            userSrv.update(user).success(function() {
+                refreshUserInfo();
+            }).error(function(data) {
+                _.each(data, function(value, key) {
+                    $scope[key + 'ErrorMsg'] = _.first(value);
+                    $scope[key + 'Error'] = true;
+                });
+            });
+        }
+
+        $scope.clear = clear;
+        $scope.update = update;
+        function refreshUserInfo() {
+            userSrv.read().success(function(data) {
+                angular.extend($scope.user, data);
+                angular.extend(userDefaults, data);
+            });
+        }
+        refreshUserInfo();
     };
-userController.$inject = ['$rootScope', '$scope', 'menus'];
+userController.$inject = ['$rootScope', '$scope', 'userSrv'];
 angular.module('adminApp').controller('UserCtrl', userController);
+
+var userService = function($http) {
+        var baseURL = '/api/v1/user/me';
+        return {
+            read: function() {
+                return $http({
+                    method: 'GET',
+                    url: baseURL
+                });
+            },
+            update: function(user) {
+                return $http({
+                    method: 'PUT',
+                    url: baseURL,
+                    data: JSON.stringify(user)
+                });
+            }
+        };
+    };
+angular.module('adminApp').factory('userSrv', ['$http', userService]);
