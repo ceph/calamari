b4ae081fde45c216868980aa1ffba84a5b378fd0
Yan-Fa Li <yanfali@gmail.com>
2013-10-28 14:53:11 -0700

    Fix http font directory
    it's relative to styles

--- ui/admin/Gruntfile.js
+++ ui/admin/Gruntfile.js
@@ -149,7 +149,7 @@ module.exports = function (grunt) {
         importPath: '<%= yeoman.app %>/bower_components',
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/styles/fonts',
+        httpFontsPath: 'fonts',
         relativeAssets: false
       },
       dist: {},
