8b721a5571fe3676c169a2a886a738794051d674
John Spray <john.spray@inktank.com>
2014-01-31 18:12:57 +0000

    Little bit of debug in EmbeddedCephCtl.shutdown

--- tests/ceph_ctl.py
+++ tests/ceph_ctl.py
@@ -1,7 +1,9 @@
+import getpass
 import logging
 import shutil
 import tempfile
 import time
+import psutil
 from minion_sim.sim import MinionSim
 from itertools import chain
 
@@ -95,6 +97,14 @@ class EmbeddedCephControl(CephControl):
         for sim in self._sims.values():
             sim.stop()
             sim.join()
+
+        log.debug("lingering processes: %s" %
+                  [p.name for p in psutil.process_iter() if p.username == getpass.getuser()])
+        # Sleeps in tests suck... this one is here because the salt minion doesn't give us a nice way
+        # to ensure that when we shut it down, subprocesses are complete before it returns, and even
+        # so we can't be sure that messages from a dead minion aren't still winding their way
+        # to cthulhu after this point.  So we fudge it.
+        time.sleep(5)
 
         for config_dir in self._config_dirs.values():
             shutil.rmtree(config_dir)
