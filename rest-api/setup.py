79e32834d5746c5760e38de15ba2ec116347f992
John Spray <john.spray@inktank.com>
2014-02-10 14:38:52 +0000

    Build fix

--- rest-api/setup.py
+++ rest-api/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="calamari-rest-api",
     version="0.1",
-    packages=['calamari_rest'],
+    packages=['calamari_rest', 'calamari_rest.urls', 'calamari_rest.views', 'calamari_rest.serializers'],
     url="http://www.inktank.com/enterprise/",
     author="Inktank Storage Inc.",
     author_email="info@inktank.com",
