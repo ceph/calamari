7698a7a107b304dc4ac1111f656824bbe719556f
John Spray <john.spray@inktank.com>
2014-04-03 13:52:36 +0100

    alembic: don't stamp on cthulhu's logging config
    The alembic logging config will still kick in if running
    from CLI rather than from within calamari-ctl.
    
    Fixes: #7379

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import logging
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -9,7 +10,9 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if 'cthulhu' not in logging.Logger.manager.loggerDict:
+    # Only set up logging if not running inside a cthulhu environment
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
