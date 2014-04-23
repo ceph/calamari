018d665086fbf3f29672d64000956d5901908930
Gregory Meno <gregory.meno@inktank.com>
2014-04-23 10:31:04 -0700

    Fixes test_mon_down test failure

--- tests/utils.py
+++ tests/utils.py
@@ -5,21 +5,23 @@ from tests.config import TestConfig
 config = TestConfig()
 
 
-def get_timeout():
-    timeout = config.get('testing', 'embedded_wait_timeout')
+def get_timeout_scaling_factor():
+    factor = 1
     if config.get('testing', 'ceph_control') == 'external':
-        timeout = config.get('testing', 'external_wait_timeout')
-
-    return timeout
+        factor = config.get('testing', 'external_timeout_factor')
+    elif config.get('testing', 'ceph_control') == 'embedded':
+        factor = config.get('testing', 'embedded_timeout_factor')
+    return factor
 
 
 class WaitTimeout(Exception):
     pass
 
 
-def wait_until_true(condition, timeout=get_timeout()):
+def wait_until_true(condition, timeout=10):
     elapsed = 0
     period = 1
+    timeout = timeout * get_timeout_scaling_factor()
     while not condition():
         if elapsed >= timeout:
             raise WaitTimeout("After %s seconds (at %s)" % (elapsed, datetime.datetime.utcnow().isoformat()))
