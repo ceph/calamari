690a21e68dea0381f7dd1839b95d843384e69afd
John Spray <jspray@redhat.com>
2014-06-09 22:28:31 +0100

    cthulhu: rename log to 'calamari'
    This is just the internal name for the log, so that
    other code (calamari_common) can get loggers called
    calamari.* and count on them being output.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- cthulhu/cthulhu/log.py
+++ cthulhu/cthulhu/log.py
@@ -5,7 +5,7 @@ config = CalamariConfig()
 
 
 FORMAT = "%(asctime)s - %(levelname)s - %(name)s %(message)s"
-log = logging.getLogger('cthulhu')
+log = logging.getLogger('calamari')
 handler = logging.FileHandler(config.get('cthulhu', 'log_path'))
 handler.setFormatter(logging.Formatter(FORMAT))
 log.addHandler(handler)
