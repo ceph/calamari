af54b33910029937e0b57f3c07e1cc63fc77ade9
Dan Mick <dan.mick@inktank.com>
2014-07-05 15:45:17 -0700

    Clean up salt-bootstrap: 1) stable works for rhel7 2) use urllib everywhere
    Signed-off-by: Dan Mick <dan.mick@inktank.com>

--- vagrant/urllib-bootstrap-salt.sh
+++ vagrant/urllib-bootstrap-salt.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -
 
+# just use urllib; tired of curl failing mid-bootstrap.  urllib always works.
+
 python \
     -c 'import urllib; print urllib.urlopen("https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.sh").read()' \
     | sh -s -- "$@"
