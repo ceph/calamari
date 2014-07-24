1c7615b01f4535b6b67c17a9abd86d437ba894ac
Gregory Meno <gregory.meno@inktank.com>
2014-07-24 12:36:40 -0700

    cthulhu: cleanup unused code in factory
    Signed-off-by: Gregory Meno <gregory.meno@inktank.com>

--- cthulhu/cthulhu/manager/crush_request_factory.py
+++ cthulhu/cthulhu/manager/crush_request_factory.py
@@ -1,25 +1,9 @@
-import logging
-
 from cthulhu.manager.request_factory import RequestFactory
 from cthulhu.manager.user_request import OsdMapModifyingRequest
 
 
-log = logging.getLogger('crush_request_factory')
-
 class CrushRequestFactory(RequestFactory):
     def update(self, osd_id, attributes):
-        commands = []
-
-        # commands.append(('osd in', {'ids': [attributes['id'].__str__()]}))
-        commands.append(('osd setcrushmap', {'data': attributes}))
-
-        log.error(str(attributes))
-        # raise RuntimeError("It is not valid to set a down OSD to be up")
-
-        if not commands:
-            # Returning None indicates no-op
-            return None
-
+        commands = [('osd setcrushmap', {'data': attributes})]
         message = "Replacing CRUSH map in {cluster_name}".format(cluster_name=self._cluster_monitor.name)
-
         return OsdMapModifyingRequest(message, self._cluster_monitor.fsid, self._cluster_monitor.name, commands)
