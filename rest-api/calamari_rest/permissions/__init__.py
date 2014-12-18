6b7aae818a32e27aa2e8e3d5eb95368c11a812ae
Gregory Meno <gmeno@redhat.com>
2014-12-18 03:55:30 -0800

    rest-api: change Allow header to reflect the client's permissions on the resource
    Signed-off-by: Gregory Meno <gmeno@redhat.com>

--- rest-api/calamari_rest/permissions/__init__.py
+++ rest-api/calamari_rest/permissions/__init__.py
@@ -7,6 +7,7 @@ class IsRoleAllowed(BasePermission):
         has_permission = False
         if request.user.groups.filter(name='readonly').exists():
             has_permission = request.method in SAFE_METHODS
+            view.headers['Allow'] = ', '.join(SAFE_METHODS)
         elif request.user.groups.filter(name='read/write').exists():
             has_permission = True
         elif request.user.is_superuser:
