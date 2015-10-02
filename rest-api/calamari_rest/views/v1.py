75f7b199b5251c56a134858a31a03ae84e2d83cd
Gregory Meno <gmeno@redhat.com>
2015-10-02 15:32:16 -0700

    rest-api: make login a noop
    Signed-off-by: Gregory Meno <gmeno@redhat.com>

--- rest-api/calamari_rest/views/v1.py
+++ rest-api/calamari_rest/views/v1.py
@@ -531,28 +531,6 @@ as follows:
 If authentication is successful, 200 is returned, if it is unsuccessful
 then 401 is returend.
     """
-    if request.method == 'POST':
-        username = request.DATA.get('username', None)
-        password = request.DATA.get('password', None)
-        msg = {}
-        if not username:
-            msg['username'] = 'This field is required'
-        if not password:
-            msg['password'] = 'This field is required'
-        if len(msg) > 0:
-            return Response(msg, status=status.HTTP_400_BAD_REQUEST)
-        user = authenticate(username=username, password=password)
-        if not user:
-            return Response({
-                'message': 'User authentication failed'
-            }, status=status.HTTP_401_UNAUTHORIZED)
-        auth_login(request, user)
-        if request.session.test_cookie_worked():
-            request.session.delete_test_cookie()
-        return Response({})
-    else:
-        pass
-    request.session.set_test_cookie()
     return Response({})
 
 
