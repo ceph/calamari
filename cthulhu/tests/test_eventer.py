28c8133a0eb9bbabc79c2d1100a9e4fc568d206a
Gregory Meno <gmeno@redhat.com>
2016-04-28 15:29:53 -0400

    add test for pool events
    Signed-off-by: Gregory Meno <gmeno@redhat.com>

--- cthulhu/tests/test_eventer.py
+++ cthulhu/tests/test_eventer.py
@@ -94,3 +94,165 @@ class TestEventer(TestCase):
         message = '\n'.join([str(x) for x in self.eventer._emit.call_args_list])
         print message
         self.assertIn('now quorum leader', message)
+
+    def test_that_it_emits_pool_events(self):
+        self.eventer._emit = MagicMock()
+        new = MagicMock()
+        old = MagicMock()
+        old.data = {}
+        old.data["pools"] = [
+            {
+                "auid": 0,
+                "cache_min_evict_age": 0,
+                "cache_min_flush_age": 0,
+                "cache_mode": "none",
+                "cache_target_dirty_high_ratio_micro": 600000,
+                "cache_target_dirty_ratio_micro": 400000,
+                "cache_target_full_ratio_micro": 800000,
+                "crash_replay_interval": 0,
+                "crush_ruleset": 0,
+                "erasure_code_profile": "",
+                "expected_num_objects": 0,
+                "fast_read": False,
+                "flags": 1,
+                "flags_names": "hashpspool",
+                "hit_set_count": 0,
+                "hit_set_params": {
+                    "type": "none"
+                },
+                "hit_set_period": 0,
+                "last_change": "7",
+                "last_force_op_resend": "0",
+                "min_read_recency_for_promote": 0,
+                "min_size": 1,
+                "min_write_recency_for_promote": 0,
+                "object_hash": 2,
+                "pg_num": 64,
+                "pg_placement_num": 64,
+                "pool": 1,
+                "pool_name": "data",
+                "pool_snaps": [],
+                "quota_max_bytes": 0,
+                "quota_max_objects": 0,
+                "read_tier": -1,
+                "removed_snaps": "[]",
+                "size": 1,
+                "snap_epoch": 0,
+                "snap_mode": "selfmanaged",
+                "snap_seq": 0,
+                "stripe_width": 0,
+                "target_max_bytes": 0,
+                "target_max_objects": 0,
+                "tier_of": -1,
+                "tiers": [],
+                "type": 1,
+                "use_gmt_hitset": True,
+                "write_tier": -1
+            }]
+
+        new.data = {}
+        new.data["pools"] = [
+            {
+                "auid": 0,
+                "cache_min_evict_age": 0,
+                "cache_min_flush_age": 0,
+                "cache_mode": "none",
+                "cache_target_dirty_high_ratio_micro": 0,
+                "cache_target_dirty_ratio_micro": 0,
+                "cache_target_full_ratio_micro": 0,
+                "crash_replay_interval": 0,
+                "crush_ruleset": 0,
+                "erasure_code_profile": "",
+                "expected_num_objects": 0,
+                "fast_read": False,
+                "flags": 1,
+                "flags_names": "hashpspool",
+                "hit_set_count": 0,
+                "hit_set_params": {
+                    "type": "none"
+                },
+                "hit_set_period": 0,
+                "last_change": "1",
+                "last_force_op_resend": "0",
+                "min_read_recency_for_promote": 0,
+                "min_size": 1,
+                "min_write_recency_for_promote": 0,
+                "object_hash": 2,
+                "pg_num": 64,
+                "pg_placement_num": 64,
+                "pool": 0,
+                "pool_name": "rbd",
+                "pool_snaps": [],
+                "quota_max_bytes": 0,
+                "quota_max_objects": 0,
+                "read_tier": -1,
+                "removed_snaps": "[]",
+                "size": 1,
+                "snap_epoch": 0,
+                "snap_mode": "selfmanaged",
+                "snap_seq": 0,
+                "stripe_width": 0,
+                "target_max_bytes": 0,
+                "target_max_objects": 0,
+                "tier_of": -1,
+                "tiers": [],
+                "type": 1,
+                "use_gmt_hitset": True,
+                "write_tier": -1
+            },
+            {
+                "auid": 0,
+                "cache_min_evict_age": 0,
+                "cache_min_flush_age": 0,
+                "cache_mode": "none",
+                "cache_target_dirty_high_ratio_micro": 600000,
+                "cache_target_dirty_ratio_micro": 400000,
+                "cache_target_full_ratio_micro": 800000,
+                "crash_replay_interval": 0,
+                "crush_ruleset": 0,
+                "erasure_code_profile": "",
+                "expected_num_objects": 0,
+                "fast_read": False,
+                "flags": 1,
+                "flags_names": "hashpspool",
+                "hit_set_count": 0,
+                "hit_set_params": {
+                    "type": "none"
+                },
+                "hit_set_period": 0,
+                "last_change": "7",
+                "last_force_op_resend": "0",
+                "min_read_recency_for_promote": 0,
+                "min_size": 1,
+                "min_write_recency_for_promote": 0,
+                "object_hash": 2,
+                "pg_num": 64,
+                "pg_placement_num": 64,
+                "pool": 1,
+                "pool_name": "data",
+                "pool_snaps": [],
+                "quota_max_bytes": 0,
+                "quota_max_objects": 0,
+                "read_tier": -1,
+                "removed_snaps": "[]",
+                "size": 1,
+                "snap_epoch": 0,
+                "snap_mode": "selfmanaged",
+                "snap_seq": 0,
+                "stripe_width": 0,
+                "target_max_bytes": 0,
+                "target_max_objects": 0,
+                "tier_of": -1,
+                "tiers": [],
+                "type": 1,
+                "use_gmt_hitset": True,
+                "write_tier": -1,
+            }]
+
+        self.eventer._on_pool_status(12345, old, old)
+        self.assertFalse(self.eventer._emit.called)
+
+        self.eventer._on_pool_status(12345, new, old)
+        self.assertIn('added to cluster', '\n'.join([str(x) for x in self.eventer._emit.call_args_list]))
+        self.eventer._on_pool_status(12345, old, new)
+        self.assertIn('removed from cluster', '\n'.join([str(x) for x in self.eventer._emit.call_args_list]))
